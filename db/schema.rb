d00e3fa0ddb4b722999f31694c4d2511a6514bc4
Andreas203 <apapakonstantinou95>
2020-01-05 14:43:19 +0000

    Cloud sql added

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_01_05_142511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+  end
 
 end
