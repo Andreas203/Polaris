e22d6a9a734e62e455900efa5c2637281be908b3
Andreas203 <apapakonstantinou95>
2020-02-21 11:12:49 +0000

    migrations and search

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_033847) do
+ActiveRecord::Schema.define(version: 2020_02_10_103105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "companies", force: :cascade do |t|
     t.string "companyName"
-  end
-
-  create_table "products", force: :cascade do |t|
+    t.string "companySymbol"
+    t.jsonb "past"
+    t.jsonb "pred"
   end
 
 end
