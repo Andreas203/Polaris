62e234abf92bb0c136767673fa85b0937b68a4d9
Andreas203 <apapakonstantinou95>
2020-03-04 14:16:19 +0000

    on

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,10 @@
 class WelcomeController < ApplicationController
   def index
     if params[:search].blank?
-      @results = ["ome","doe","trese"]
+      @results = Company.all
     else
-      if @results.nil?
-        @results = ["ome","doe","trese"]
-      else
-        @parameter = params[:search]
-        @results = Company.where(companyName: @parameter).or(Company.where(companySymbol: @parameter))
-      end
+      @parameter = params[:search]
+      @results = Company.where(companyName: @parameter).or(Company.where(companySymbol: @parameter))
     end
   end
 end
