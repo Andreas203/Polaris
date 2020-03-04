06f45cc28427fc4ef3cc849815635caf130afb98
James Evans <jamesevans963@gmail.com>
2020-03-04 15:15:38 +0000

    new layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    if params[:search].blank? && !(Company.first.nil?)
+    if !(Company.first.nil?)
       @results = Company.all
-    elsif !(Company.first.nil?)
-      @parameter = params[:search]
-      @results = Company.where(companyName: @parameter).or(Company.where(companySymbol: @parameter))
     else
       @results = ["two", "four", "tres"]
     end
